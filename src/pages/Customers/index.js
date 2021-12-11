8733b994f138026cc37aa65d217a5346c074c274
Nataliapine <nataliapine@icloud.com>
2021-12-11 11:20:05 -0300

    customer database registered.

--- src/pages/Customers/index.js
+++ src/pages/Customers/index.js
@@ -1,6 +1,8 @@
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
+import firebase from '../../services/firebaseConnection';
+import { toast } from 'react-toastify';
 
 import "./customers.scss" ;
 
@@ -11,25 +13,44 @@ export default function Customers() {
 
     function handleAdd(e) {
         e.preventDefault();
-        alert('teste')
+        
+        if(companyName !== '' && cnpj !== '' && address !== '') {
+            firebase.firestore().collection('customers')
+            .add({
+                companyName: companyName,
+                cnpj: cnpj,
+                address: address
+            })
+            .then(() => {
+                setCompanyName('');
+                setCnpj('');
+                setAddress('');
+                toast.info('Saved with Sucess!');
+            })
+            .catch((error) => {
+                console.log('error')
+                toast.error('Sorry, Something went wrong!');
+            }) 
+        }else {
+            toast.error('fill in all fields')
+        }
     }
 
     return (
         <div>
             <Header />
             <div className="content">
-                    <Title name="My Profile"></Title>
+                <Title name="Customers"></Title>
             </div>
-            <h1>Customers</h1>
             <div className="container">
                 <div className='card'>
                     <form className='form-profile' onSubmit={handleAdd} >
                         <label>Company Name</label>
-                        <input type="text" value={companyName} onChange={ (e) => setCompanyName(e.target.value)} />
+                        <input type="text" placeholder='Company Name' value={companyName} onChange={ (e) => setCompanyName(e.target.value)} />
                         <label>CNPJ</label>
-                        <input type="text" value={cnpj} onchange={ (e) => setCnpj(e.target.value)} />
+                        <input type="text" placeholder='CNPJ' value={cnpj} onChange={ (e) => setCnpj(e.target.value)} />
                         <label>Address</label>
-                        <input type="text" value={address} onchange={ (e) => setAddress(e.target.value)} />
+                        <input type="text" placeholder='your address' value={address} onChange={ (e) => setAddress(e.target.value)} />
                         <button type="submit">Register</button>
                     </form>
                 </div>   
