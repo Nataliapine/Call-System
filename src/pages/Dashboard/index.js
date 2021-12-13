c48bcee743cdabd47c7c8e2a0d2dab4885d1b7bc
Nataliapine <nataliapine@icloud.com>
2021-12-13 11:52:59 -0300

    added function updateState and loadCalls
    to create the list of calls
    load list of calls

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,67 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import "./dashboard.scss";
 
 import Header from '../../components/Header';
 import Title from "../../components/Title";
 import { Link } from "react-router-dom";
 import { FiEdit2, FiPlus, FiSearch } from 'react-icons/fi';
+import firebase from '../../services/firebaseConnection';
+import { format } from 'date-fns';
+
+
+const listRef = firebase.firestore().collection('calls').orderBy('created', 'desc');
 
 export default function Dashboard() {
 
     const [ calls, setCalls ] = useState([1]); 
+    const [ loading, setLoading ] = useState(true);
+    const [ loadingMore, setLoadingMore ] = useState(false);
+    const [ isEmpty, setIsEmpty ] = useState(false);
+    const [ lastDocs, setLastDocs] = useState();
+
+    useEffect(() => {
+        loadCalls();
+        return () => {
+
+        }
+    }, []);
+    async function loadCalls() {
+        await listRef.limit(5)
+        .get()
+        .then((snapshot)=> {})
+        .catch((err) => {
+            console.log('Something went wrong', err);
+            setLoadingMore(false)
+
+        })
+        setLoading(false);
+    }
+
+    async function updateState(snapshot) {
+        const isCollectionEmpty = snapshot.size === 0;
+
+        if(!isCollectionEmpty) {
+            let lista = [];
+
+            snapshot.forEach((doc)=> {
+                lista.push({
+                    id:doc.id,
+                    subject:doc.data().subject,
+                    client: doc.data().client,
+                    clientId: doc.data().clientId,
+                    created: doc.data().created,
+                    createdFormated: format(doc.data().created.toDate(), 'dd/mm/yyyy'),
+                    status: doc.data().status,
+                    comment: doc.data().comment
+
+                })
+            })
+            const lastDoc = snapshot.docs[snapshot.docs.length - 1];
+            setCalls(calls => [...calls, ...lista]);
+            setLastDocs(lastDoc);
+
+        }
+    }
 
     return(
         <div>
